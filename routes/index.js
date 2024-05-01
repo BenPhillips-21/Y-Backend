31b73064915a9bf93457799f0d1b5fc89aa214aa
Ben Phillips <pben012@gmail.com>
2024-05-01 11:42:59 +1000

    added userSignUp and userLogin routes and middleware

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const user_controller = require('../controllers/userController')
+const post_controller = require('../controllers/postController')
+const comment_controller = require('../controllers/commentController')
+
+const passport = require("passport");
+const jwtStrategy = require("../strategies/jwt")
+
+passport.use(jwtStrategy);
+
+router.post('/sign-up', user_controller.userSignUp)
+
+router.post('/login', user_controller.userLogin)
 
 module.exports = router;
---
+++ strategies/jwt.js
@@ -0,0 +1,20 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/userModel');
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+require('dotenv').config();
+opts.secretOrKey = process.env.secret
+
+module.exports = new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+        const user = await User.findOne({ _id: jwt_payload.userId });
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    } catch (error) {
+        return done(error, false);
+    }
+});
